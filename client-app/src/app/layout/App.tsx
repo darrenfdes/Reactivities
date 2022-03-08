11db9a67080088ebc7ff22e64ed1d51552f4a6a9
darrenmfdes <darrenmfdes@gmail.com>
2022-03-08 15:34:18 +0530

    form and edit sections elements created

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { Activity } from "../models/Activity";
+import Navbar from "./Navbar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
+
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((res) => {
+        setActivities(res.data);
+      });
+  }, []);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find((x) => x.id === id));
+  };
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined);
+  };
+
+  const handleFormOpen = (id?: string) => {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  };
+
+  const handleFormClose = () => {
+    setEditMode(false);
+  };
+
+  return (
+    <>
+      <Navbar openForm={handleFormOpen} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+        />
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/Activity";
+
+interface Props {
+  activity: Activity | undefined;
+  closeForm: () => void;
+}
+
+const ActivityForm = ({ activity, closeForm }: Props) => {
+  return (
+    <Segment clearing>
+      <Form>
+        <Form.Input placeholder="Title" />
+        <Form.Input placeholder="Description" />
+        <Form.Input placeholder="Category" />
+        <Form.Input placeholder="Date" />
+        <Form.Input placeholder="City" />
+        <Form.Input placeholder="Venue" />
+
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          onClick={closeForm}
+          floated="right"
+          type="submit"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
+};
+
+export default ActivityForm;
